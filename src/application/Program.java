4fa183944823fe97f7e9848a9a42558fed7b514a
Avonts <vinialves022@gmail.com>
2020-04-06 23:01:45 -0300

    Adicionando SellerDao e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,9 @@
 package application;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,13 +11,15 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(1, "Vinícius", "vinialves022@gmail.com", new Date(), 2000.0, obj);
 		
 		System.out.println(obj);
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		
 	}
 
 }
