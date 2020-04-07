245c9a908abbea5716111ac5f972ae529793617b
Avonts <vinialves022@gmail.com>
2020-04-07 17:50:30 -0300

    Implementação do Seller Insert

---
+++ src/application/ExFindAll.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class ExFindAll {
+
+	public static void main(String[] args) {
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("=== TEST 1: Seller FindByID ===");
+		Seller seller = sellerDao.findById(2);
+		System.out.println(seller);
+		
+		System.out.println("\n=== TEST 2: Seller FindByDepartment ===");
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		
+		list.forEach(System.out::println);
+		
+		System.out.println("\n=== TEST 3: Seller FindAll ===");
+		List<Seller> list2 = sellerDao.findAll();
+		
+		list2.forEach(System.out::println);
+		
+	}
+
+}
---
+++ src/application/ExSellerInsert.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.Date;
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class ExSellerInsert {
+
+	public static void main(String[] args) {
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("=== TEST 1: Seller FindByID ===");
+		Seller seller = sellerDao.findById(2);
+		System.out.println(seller);
+		
+		System.out.println("\n=== TEST 2: Seller FindByDepartment ===");
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		
+		list.forEach(System.out::println);
+		
+		System.out.println("\n=== TEST 3: Seller FindAll ===");
+		List<Seller> list2 = sellerDao.findAll();
+		
+		list2.forEach(System.out::println);
+		
+		System.out.println("\n=== TEST 4: Seller Insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id: " + newSeller.getId());
+		
+
+	}
+
+}
