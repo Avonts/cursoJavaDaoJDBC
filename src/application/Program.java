0fb932378d1bbcef8d508dc6a9a8a26e315a5edf
Avonts <vinialves022@gmail.com>
2020-04-06 22:05:35 -0300

    Adicionando a classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,22 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
+		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(1, "Vinícius", "vinialves022@gmail.com", new Date(), 2000.0, obj);
+		
+		System.out.println(obj);
+		System.out.println(seller);
 	}
 
 }
